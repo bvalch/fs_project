d79d2d12c8644521d583937a250ea299d1294882
Bozhidar Valchev <bvalch@gmail.com>
2022-09-02 17:04:29 +0100

    git not working again

--- client/src/container/TopContainer.js
+++ client/src/container/TopContainer.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { getDinos } from "../Components/dinoService";
 
 
+
 const TopContainer = () => {
-    const [dinos, setDinos] = useState([])
+    const [dinos, setDinos] = useState([]);
 
 
     useEffect(() => {
         getDinos().then((data)=>setDinos(data));
+    }, []);
 
-    }, [])
+
+
+
+
 
     
     return(
