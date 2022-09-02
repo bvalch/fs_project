842c638d7041393f3bfbbf010f4355cfc70860ee
Bozhidar Valchev <bvalch@gmail.com>
2022-09-02 12:13:28 +0100

    mongo route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const MongoClient=require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
+const createRouter=require("./helpers/createRouter")
+
 app.use(cors());
 app.use(express.json());
 
 
 
+MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
+    .then((client) => {
+        const db = client.db('dinoProject');
+        const dinosCollection = db.collection('dinos');
+        const dinosRouter = createRouter(dinosCollection);
+        app.use("/api/dinos", dinosRouter)
+
+
+    }).catch((error) => console.log(error))
+
+
+app.listen(9000, function () { console.log(`!!!!!listening on port ${this.address().port}`) })
