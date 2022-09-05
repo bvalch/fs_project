d77953dc818945c1405708077fd985b8121eca1a
bvalch <108139471+bvalch@users.noreply.github.com>
2022-09-05 18:21:44 +0100

    Revert "quiz basics set up"

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -13,7 +13,6 @@ return(
         <li><Link to="/randomdino">Random Dino</Link> </li>
         <li><Link to="/dinolist">All Dinos</Link> </li> 
         <li><Link to="/favdinos">My Dinos</Link> </li> 
-        <li><Link to="/quiz">Take a quiz</Link> </li> 
     </ul>
     
     )
