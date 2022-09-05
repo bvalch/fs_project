5004bc746b36d552a8875a2f43d7e4baa34d8afe
Bozhidar Valchev <bvalch@gmail.com>
2022-09-05 19:10:19 +0100

    mergin quiz

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,31 +1,36 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './style/NavBar.css';
 
 
-const NavBar=()=>{
+const NavBar = () => {
 
-return(
-    
-    
-    <nav className='navbar'>
-        <button className='button'>
-            <Link className='link' to="/">Home</Link> 
-        </button>
-        <button className='button'>
-            <Link className='link' to="/finddino">Look up Dino</Link> 
-        </button>
-        <button className='button'>
-            <Link className='link' to="/randomdino">Random Dino</Link> 
+    return (
+
+
+        <nav className='navbar'>
+            <button className='button'>
+                <Link className='link' to="/">Home</Link>
             </button>
-        <button className='button'>
-            <Link className='link' to="/dinolist">All Dinos</Link> 
-        </button> 
-        <button className='button'>
-            <Link className='link' to="/favdinos">My Dinos</Link> 
-        </button> 
-    </nav>
-    
+            <button className='button'>
+                <Link className='link' to="/finddino">Look up Dino</Link>
+            </button>
+            <button className='button'>
+                <Link className='link' to="/randomdino">Random Dino</Link>
+            </button>
+            <button className='button'>
+                <Link className='link' to="/dinolist">All Dinos</Link>
+            </button>
+            <button className='button'>
+                <Link className='link' to="/favdinos">My Dinos</Link>
+            </button>
+            <button className='button'>
+                <Link to="/quiz">Take a quiz</Link>
+            </button>
+
+            <Link to="/quiz">Take a quiz</Link>
+        </nav>
+
     )
 
 
